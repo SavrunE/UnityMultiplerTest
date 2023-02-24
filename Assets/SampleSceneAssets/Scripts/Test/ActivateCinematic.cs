3c47c7e36f6784f7725b97b7844de22a8f21b218
SavrunE <savransky@voltep.ru>
2023-02-25 02:55:15 +0300

    start

---
+++ Assets/SampleSceneAssets/Scripts/Test/ActivateCinematic.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivateCinematic : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector pd;
+
+    private void Start()
+    {
+        //pd.Play();
+
+	}
+}
